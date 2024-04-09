9b716be67866adbf225405690c92d5f2821c8374
Rakshitha.D <rakshithagowda.d@gmail.com>
2024-04-09 20:05:09 +0530

    created pydantic model for patch operation and made patch operation dynamic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+from typing import Optional
 from fastapi import FastAPI, HTTPException,status
 from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from datetime import datetime
 
 app = FastAPI()
 
@@ -11,6 +13,13 @@ class Record(BaseModel):
     type: str
     name: str
     updated_date: str
+
+class UpdateRecord(BaseModel):
+    id: str | None = None
+    dataset_id: str | None = None
+    type: str | None = None
+    name: str | None = None
+    updated_date: str 
 
 conn = psycopg2.connect(host='localhost',database='obsrv',user='postgres',password='drakshitha',cursor_factory=RealDictCursor)
 cursor = conn.cursor()
@@ -40,17 +49,25 @@ def create_records(record: Record):
     return {"record_inserted": new_record}
 
 @app.patch("/v1/dataset/{dataset_id}")
-def update_record(dataset_id,record: Record):
-    cursor.execute("""UPDATE datasets set name=%s WHERE dataset_id=%s """,(record.name,dataset_id))
+def update_record(dataset_id,record: UpdateRecord):
+    update_fields = ", ".join(f"{field}=%s" for field in record.model_dump(exclude_unset=True))
+    update_values = [getattr(record, field) for field in record.model_dump(exclude_unset=True)]
+    update_values.append(dataset_id)
+    print(update_fields)
+    print(update_values)
+    cursor.execute(f"""UPDATE datasets SET {update_fields} WHERE dataset_id = %s RETURNING *""", update_values)
     updated_record = cursor.fetchone()
     conn.commit()
+    #cursor.execute("""UPDATE datasets set name=%s WHERE dataset_id=%s returning *""",(record.name,dataset_id))
+    #updated_record = cursor.fetchone()
+    #conn.commit()
     if updated_record == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"record with id: {id} does not exist")
     return {"record": updated_record}
 
-@app.delete("/v1/dataset/{id}")
-def delete_record(id):
-    cursor.execute("""DELETE FROM datasets where id = %s returning *""",(id,))
+@app.delete("/v1/dataset/{dataset_id}")
+def delete_record(dataset_id):
+    cursor.execute("""DELETE FROM datasets where id = %s returning *""",(dataset_id,))
     deleted_post=cursor.fetchone()
     conn.commit()
     return {"deleted_post": deleted_post}
