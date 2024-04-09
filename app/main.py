32a82dfda371f0645254e064c654eb874274ad6d
Rakshitha.D <rakshithagowda.d@gmail.com>
2024-04-09 15:35:57 +0530

    created post operation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import datetime
 from fastapi import FastAPI, HTTPException,status
 from pydantic import BaseModel
 import psycopg2
@@ -11,6 +10,7 @@ class Record(BaseModel):
     dataset_id: str
     type: str
     name: str
+    updated_date: str
 
 conn = psycopg2.connect(host='localhost',database='obsrv',user='postgres',password='drakshitha',cursor_factory=RealDictCursor)
 cursor = conn.cursor()
@@ -23,3 +23,18 @@ def get_record(dataset_id):
     if not record:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"record with id: {dataset_id} was not found")
     return {"record_details": record}
+
+@app.get("/")
+def get_all_records():
+    cursor.execute("""SELECT * FROM datasets""")
+    records= cursor.fetchall()
+    if not records:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="no records found")
+    return {"records": records}
+
+@app.post("/v1/dataset")
+def create_records(record: Record):
+    cursor.execute("""insert into datasets (id,dataset_id,type,name,updated_date) values (%s,%s,%s,%s,%s) returning * """,(record.id,record.dataset_id,record.type,record.name,record.updated_date))
+    new_record = cursor.fetchone()
+    conn.commit()
+    return {"record_inserted": new_record}
