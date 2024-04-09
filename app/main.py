c6d149aea4b177077389641d47927bb1a3a25c42
Rakshitha.D <rakshithagowda.d@gmail.com>
2024-04-09 13:30:37 +0530

    implemented get operation using dataset_id

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException,status
 from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -17,5 +17,10 @@ cursor = conn.cursor()
 print("Database connection was succesfull")
 
 @app.get("/v1/dataset/{dataset_id}")
-def get_record():
-    return {"Hello": "World"}
+def get_record(dataset_id):
+    print(type(dataset_id))
+    cursor.execute(f"""SELECT * FROM datasets where dataset_id = '{dataset_id}' """)
+    record = cursor.fetchone()
+    if not record:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"record with id: {dataset_id} was not found")
+    return {"record_details": record}
