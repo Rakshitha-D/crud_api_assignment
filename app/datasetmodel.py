a020132a8b622fa0b1057e72aef8da68d0785393
Rakshitha.D <rakshithagowda.d@gmail.com>
2024-04-10 18:09:28 +0530

    updated post and patch to take all fields

--- app/datasetmodel.py
+++ app/datasetmodel.py
@@ -1,16 +1,39 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel,Field
+from typing import Any, Dict, List, Optional
+from datetime import datetime
 
 class Dataset(BaseModel):
     id: str
     dataset_id: str
     type: str
     name: str
-    updated_date: str
+    validation_config: dict
+    extraction_config: dict
+    dedup_config: dict
+    data_schema: dict
+    denorm_config: dict
+    router_config: dict
+    dataset_config: dict
+    status: str
+    tags: List[str]
+    data_version: int
+    created_by: str
+    updated_by: str
 
 class UpdateDataset(BaseModel):
-    id: Optional[str]= None
-    dataset_id: str | None = None
+    #id: Optional[str]= None
+    #dataset_id: str | None = None
     type: str | None = None
     name: str | None = None
-    updated_date: str 
+    validation_config: dict | None = None
+    extraction_config: dict | None = None
+    dedup_config: dict | None = None
+    data_schema: dict | None = None
+    denorm_config: dict | None = None
+    router_config: dict | None = None
+    dataset_config: dict | None = None
+    status: str | None = None
+    tags: List[str] | None = None
+    data_version: int | None = None
+    created_by: str | None = None
+    updated_by: str | None = None
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException,status
 from datetime import datetime
 from .database import connection
 from .datasetmodel import Dataset,UpdateDataset
+import json
 app = FastAPI()
 
 
@@ -23,17 +24,42 @@ def get_all_records():
 
 @app.post("/v1/dataset",status_code=status.HTTP_201_CREATED)
 def create_records(dataset: Dataset):
-    connection.cursor.execute("""insert into datasets (id,dataset_id,type,name,updated_date) values (%s,%s,%s,%s,%s) returning * """,(dataset.id,dataset.dataset_id,dataset.type,dataset.name,dataset.updated_date))
+    updated_date = datetime.now()
+    insert_fields=", ".join(f"{field}" for field in dataset.model_dump())
+    print(insert_fields)
+    print("length:",len(insert_fields))
+    #insert_values = [json.dumps(getattr(dataset, field)) for field in dataset.model_dump() if isinstance(getattr(dataset, field), dict)]
+    insert_values = []
+    for field in dataset.model_dump():
+        attribute = getattr(dataset, field)
+        if isinstance(attribute, dict):
+            insert_values.append(json.dumps(attribute))
+        else:
+            insert_values.append(attribute)
+
+    insert_values.append(updated_date)
+    #connection.cursor.execute("""insert into datasets (id,dataset_id,type,name,validation_config,extraction_config,dedup_config,data_schema,denorm_config,router_config,dataset_config,status,tags,data_version,created_by,updated_by,updated_date) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) returning * """,insert_values)
+    connection.cursor.execute(f"""insert into datasets ({insert_fields},updated_date) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) returning * """,insert_values)
     new_dataset = connection.cursor.fetchone()
     connection.conn.commit()
-    return {"dataset_inserted": new_dataset}
+    return {"dataset_id": dataset.dataset_id}
 
 @app.patch("/v1/dataset/{dataset_id}")
 def update_record(dataset_id,dataset: UpdateDataset):
     update_fields = ", ".join(f"{field}=%s" for field in dataset.model_dump(exclude_unset=True))
-    update_values = [getattr(dataset, field) for field in dataset.model_dump(exclude_unset=True)]
+    #update_values = [getattr(dataset, field) for field in dataset.model_dump(exclude_unset=True)]
+    update_values = []
+    for field in dataset.model_dump(exclude_unset=True):
+        attribute = getattr(dataset, field)
+        if isinstance(attribute, dict):
+            update_values.append(json.dumps(attribute))
+        else:
+            update_values.append(attribute)
+
+    updated_date= datetime.now()
+    update_values.append(updated_date)
     update_values.append(dataset_id)
-    connection.cursor.execute(f"""UPDATE datasets SET {update_fields} WHERE dataset_id = %s RETURNING *""", update_values)
+    connection.cursor.execute(f"""UPDATE datasets SET {update_fields},updated_date = %s WHERE dataset_id = %s RETURNING *""", update_values)
     updated_dataset = connection.cursor.fetchone()
     connection.conn.commit()
     #cursor.execute("""UPDATE datasets set name=%s WHERE dataset_id=%s returning *""",(record.name,dataset_id))
@@ -41,7 +67,7 @@ def update_record(dataset_id,dataset: UpdateDataset):
     #conn.commit()
     if updated_dataset == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"dataset with dataset_id: {dataset_id} does not exist")
-    return {"updated_dataset_id": dataset_id}
+    return {"dataset_id": dataset_id}
 
 @app.delete("/v1/dataset/{dataset_id}")
 def delete_record(dataset_id):
@@ -50,4 +76,4 @@ def delete_record(dataset_id):
     connection.conn.commit()
     if deleted_record ==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"dataset with dataset_id: {dataset_id} does not exist")
-    return {"deleted_dataset_id": dataset_id}
+    return {"dataset_id": dataset_id}
